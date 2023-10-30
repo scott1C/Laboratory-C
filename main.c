e1bf115a096fcc31da8dd1aafd2161fdf0aa0bca
scott1q <cvasiuc.dima@gmail.com>
2023-10-30 18:53:29 +0200

    Laboratory Work #5

--- main.c
+++ main.c
@@ -1,69 +1,222 @@
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 #include <conio.h>
+#include <string.h>
 
-void isRotation(char firstString[], char secondString[])
+#define MAX_LENGTH_OF_CHAR 100
+const int size = 2;
+
+typedef struct
 {
-    int firstNumber = 0, secondNumber = 0;
-    for (int i = 0; i < strlen(firstString); i++)
-        firstNumber += firstString[i];
-    for (int i = 0; i < strlen(secondString); i++)
-        secondNumber += secondString[i];
+    int type;
+    union
+    {
+        int intData;
+        float floatData;
+        char charData[MAX_LENGTH_OF_CHAR];
+    } data;
+} StackItem;
 
-    if (firstNumber == secondNumber)
+typedef struct
+{
+    StackItem *data;
+    int top;
+    unsigned int capacity;
+} Stack;
+
+void initializeStack(Stack *stack)
+{
+    if (size < 0)
     {
-        printf("It is possible\n");
+        printf("Size of stack cannot be less than 0 \n");
+        system("pause");
+        exit(1);
     }
-    else
+    stack->data = calloc(size, sizeof(StackItem));
+    if (stack->data == NULL)
     {
-        printf("It is not possible\n");
+        printf("System cannot allocate this amount of memory space from heap! \n");
+        system("pause");
+        exit(1);
     }
+    stack->capacity = size;
+    stack->top = -1;
 }
 
-int isPalindrome(char string[])
+int isFull(Stack *stack)
 {
-    for (int i = 0; i < strlen(string) / 2; i++)
-        if (string[i] != string[strlen(string) - i - 1])
-            return 0;
-    return 1;
+    return stack->capacity - 1 == stack->top;
+}
+
+int isEmpty(Stack *stack)
+{
+    return stack->top == -1;
+}
+
+void pushToStack(Stack *stack, StackItem *value)
+{
+    int position = stack->top + 1;
+    stack->data[position] = *value;
+    stack->top++;
+}
+
+void popStack(Stack *stack)
+{
+    stack->top = stack->top - 1;
+}
+
+void peekStack(Stack *stack)
+{
+    StackItem item = stack->data[stack->top];
+    if (item.type == 1)
+        printf("%d\n", item.data.intData);
+    else if (item.type == 2)
+        printf("%f\n", item.data.floatData);
+    else if (item.type == 3)
+        printf("%s\n", item.data.charData);
+}
+
+void showStack(Stack *stack)
+{
+    printf("Stack Contents: ");
+    if (stack->top > -1)
+    {
+        for (int i = 0; i <= stack->top; i++)
+        {
+            StackItem item = stack->data[i];
+            if (item.type == 1)
+                printf("%d ", item.data.intData);
+            else if (item.type == 2)
+                printf("%f ", item.data.floatData);
+            else if (item.type == 3)
+                printf("%s ", item.data.charData);
+        }
+    }
+    else
+        printf("(empty)");
+    printf("\n");
+}
+
+void operationsMenu()
+{
+    printf("Stack Operations Menu:\n");
+    printf("1. Push\n2. Pop\n3. Peek\n4. IsEmpty\n5. IsFull\n6. Exit\n");
+    printf("Enter your choice: ");
+}
+
+void clearOutput()
+{
+    system("cls");
 }
 
 int main()
 {
-    char text[100], symbol;
-    printf("Enter your text: ");
-    fgets(text, 100, stdin);
-    text[strlen(text) - 1] = '\0';
+    Stack stack;
+    initializeStack(&stack);
+    int n = 1;
 
-    printf("Enter the symbol which you want to find: ");
-    scanf("%c", &symbol);
-    int count = 0;
-    for (int i = 0; i < strlen(text); i++)
-        if (text[i] == symbol)
-            count++;
-    printf("%d\n", count);
-
-    char one[100], two[200];
-    printf("Enter the first string: ");
-    scanf("%s", one);
-
-    printf("Enter the second string: ");
-    scanf("%s", two);
-    isRotation(one, two);
-
-    char palindromicText[100];
-    printf("Enter the string which you want to check if it is palindromic: ");
-    scanf("%s", text);
-
-    int palindrome = isPalindrome(palindromicText);
-
-    if (palindrome)
+    while (n)
     {
-        printf("It is palindrome\n");
-    }
-    else
-    {
-        printf("It is not palindrome\n");
+        int operation;
+        operationsMenu();
+        scanf("%d", &operation);
+        if (operation == 1)
+        {
+            clearOutput();
+            if (!isFull(&stack))
+            {
+                StackItem item;
+                printf("Type 1 for integer, 2 for float or 3 for char: ");
+                scanf("%d", &item.type);
+                if (item.type == 1)
+                {
+                    printf("Enter the value to push onto the stack: ");
+                    scanf("%d", &item.data.intData);
+                    clearOutput();
+                    printf("Element %d pushed onto the stack\n", item.data.intData);
+                }
+                else if (item.type == 2)
+                {
+                    printf("Enter the value to push onto the stack: ");
+                    scanf("%f", &item.data.floatData);
+                    clearOutput();
+                    printf("Element %f pushed onto the stack\n", item.data.floatData);
+                }
+                else if (item.type == 3)
+                {
+                    printf("Enter the value to push onto the stack: ");
+                    scanf("%s", item.data.charData);
+                    clearOutput();
+                    printf("Element %s pushed onto the stack\n", item.data.charData);
+                }
+                else
+                {
+                    clearOutput();
+                    printf("You entered an incorect number, try again \n");
+                    continue;
+                }
+                pushToStack(&stack, &item);
+                showStack(&stack);
+            }
+            else
+            {
+                printf("The Stack is full, you cannot enter more elements!\n");
+            }
+        }
+        else if (operation == 2)
+        {
+            clearOutput();
+            if (isEmpty(&stack))
+            {
+                printf("The stack is empty, you are not allowed to do this operation!\n");
+            }
+            else
+            {
+                printf("Popped element: ");
+                peekStack(&stack);
+                popStack(&stack);
+                showStack(&stack);
+            }
+        }
+        else if (operation == 3)
+        {
+            clearOutput();
+            if (isEmpty(&stack))
+            {
+                printf("The stack is empty, you are not allowed to do this operation!\n");
+            }
+            else
+            {
+                printf("Peeked element: ");
+                peekStack(&stack);
+            }
+        }
+        else if (operation == 4)
+        {
+            clearOutput();
+            if (isEmpty(&stack))
+                printf("The Stack is empty\n");
+            else
+                printf("The Stack is NOT empty\n");
+        }
+        else if (operation == 5)
+        {
+            clearOutput();
+            if (isFull(&stack))
+                printf("The Stack is full\n");
+            else
+                printf("The Stack is NOT full\n");
+        }
+        else if (operation == 6)
+        {
+            printf("Exiting the program");
+            n = 0;
+            free(stack.data);
+        }
+        else
+        {
+            printf("You entered the wrong number, try again\n");
+        }
     }
 
     getch();

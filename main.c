2440315f3dacea723bd0f03bf236194c0a03626b
scott1q <cvasiuc.dima@gmail.com>
2024-04-07 22:41:42 +0300

    Added little changes

--- main.c
+++ main.c
@@ -1,13 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void neighborsSum(int matrix[][100], unsigned n, unsigned m)
+int numberOfPairs = 0, minPairsInRow = 0, maxPairsInRow = 0;
+int rowWithMinPairs = 0, rowWithMaxPairs = 0;
+
+void neighborsSum(int matrix[][100], unsigned n, unsigned m, int S)
 {
-    int isSuchPairs = 0;
-    int S;
-    printf("Enter the sum S: ");
-    scanf("%d", &S);
+    int pairs = 0;
     for (unsigned i = 0; i < n; i++)
+    {
+        int currentPairs = 0;
         for (unsigned j = 0; j < m / 2; j++)
         {
             int sum = 0;
@@ -15,12 +17,24 @@ void neighborsSum(int matrix[][100], unsigned n, unsigned m)
                 sum = matrix[i][j] + matrix[i][j + 2];
             if (sum == S)
             {
-                isSuchPairs = 1;
                 printf("R%d pairs: \na[%d][%d]=%d + a[%d][%d]=%d\n\n", i + 1, i, j, matrix[i][j], i, j + 2, matrix[i][j + 2]);
+                pairs++;
+                currentPairs++;
             }
         }
-    if (!isSuchPairs)
-        printf("There is not such elements");
+        if (currentPairs > maxPairsInRow)
+        {
+            maxPairsInRow = currentPairs;
+            rowWithMaxPairs = i;
+        }
+        if (currentPairs < minPairsInRow)
+        {
+            minPairsInRow = currentPairs;
+            rowWithMinPairs = i;
+        }
+    }
+    if (!pairs)
+        printf("There is not such elements\n");
 }
 
 int main()
@@ -37,7 +51,10 @@ int main()
             printf("Enter a[%d][%d]: ", i, j);
             scanf("%d", &a[i][j]);
         }
-    // neighborsSum(a, n, m);
+    int S;
+    printf("Enter the sum S: ");
+    scanf("%d", &S);
+    // neighborsSum(a, n, m, S);
     system("pause");
     return 0;
 }
